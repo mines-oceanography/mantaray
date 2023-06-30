9c7d4971c94c716a53b10a189a44191890c2adee
Bryce Irving <bryceirving@mines.edu>
2023-06-30 13:36:24 -0600

    doc: add documentation to error module and enum

--- src/error.rs
+++ src/error.rs
@@ -1,9 +1,15 @@
+//! Custom enum Error defining ray_tracing specific errors.
+//! 
+//! The enum depends on thiserror::Error.
+
 #[derive(Debug, thiserror::Error)]
 pub(crate) enum Error {
    #[error("Undefined error")]
-   /// The ode_solvers integrate method can return an error.
+   /// The undefined error will be used as a placeholder for all other errors.
+   /// Two known errors that are currently undefined but need their own instance
+   /// in the future are an index out of bounds error and an integration error.
    Undefined,
    #[error("Argument passed was out of bounds")]
-   /// The value k = |(kx, ky)| can only be positive.
+   /// The value k = |(kx, ky)| can only be positive. If k <=0, the function will pass ArgumentOutOfBounds.
    ArgumentOutOfBounds
 }
