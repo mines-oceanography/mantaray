8f8bd949bde72feb403a392ba870d1aa573022a6
Bryce Irving <bryceirving@mines.edu>
2023-07-25 16:46:36 -0600

    feat: use references in function signatures

--- src/cartesian_coor.rs
+++ src/cartesian_coor.rs
@@ -57,7 +57,7 @@ impl CartesianFile {
     /// 
     /// # Panics
     /// This function will panic if direction is not either `"x"` or `"y"`
-    fn nearest(&self, target: f32, direction: &str) -> usize {
+    fn nearest(&self, target: &f32, direction: &str) -> usize {
         let arr = match direction {
             "x" => &self.variables.0,
             "y" => &self.variables.1,
@@ -94,8 +94,8 @@ impl CartesianFile {
     /// This function will never panic, but if given an out of bounds point,
     /// it will return the closest edge.
     fn nearest_point(&self, x: &f32, y: &f32) -> (usize, usize) {
-        let indy = self.nearest(*y, "y");
-        let indx = self.nearest(*x, "x");
+        let indy = self.nearest(y, "y");
+        let indx = self.nearest(x, "x");
         (indx, indy)
     }
 
@@ -115,13 +115,13 @@ impl CartesianFile {
     /// # Panics
     /// This function will not panic, but be aware that it can return values
     /// that are out of bounds to the array.
-    fn four_corners(&self, indx: usize, indy: usize) -> Vec<(usize, usize)> {
+    fn four_corners(&self, indx: &usize, indy: &usize) -> Vec<(usize, usize)> {
         // clockwise in order
         vec![
-            (indx, indy+1),
-            (indx+1, indy),
-            (indx, indy-1),
-            (indx-1, indy)
+            (*indx, indy+1),
+            (indx+1, *indy),
+            (*indx, indy-1),
+            (indx-1, *indy)
         ]
     }
     
@@ -142,10 +142,10 @@ impl CartesianFile {
     /// array. This function will also panic if `interpolator::bilinear` panics.
     fn interpolate(&self, points: &Vec<(usize, usize)>, target: &(f64, f64)) -> f64 {
         let pts = vec![
-            (points[0].0 as i32, points[0].1 as i32, self.depth_from_arr(points[0].0, points[0].1)),
-            (points[1].0 as i32, points[1].1 as i32, self.depth_from_arr(points[1].0, points[1].1)),
-            (points[2].0 as i32, points[2].1 as i32, self.depth_from_arr(points[2].0, points[2].1)),
-            (points[3].0 as i32, points[3].1 as i32, self.depth_from_arr(points[3].0, points[3].1)),
+            (points[0].0 as i32, points[0].1 as i32, self.depth_from_arr(&points[0].0, &points[0].1)),
+            (points[1].0 as i32, points[1].1 as i32, self.depth_from_arr(&points[1].0, &points[1].1)),
+            (points[2].0 as i32, points[2].1 as i32, self.depth_from_arr(&points[2].0, &points[2].1)),
+            (points[3].0 as i32, points[3].1 as i32, self.depth_from_arr(&points[3].0, &points[3].1)),
         ];
         interpolator::bilinear(&pts, target)
     }
@@ -164,7 +164,7 @@ impl CartesianFile {
     /// 
     /// # Panics
     /// This function will panic if combined index is out of bounds.
-    fn depth_from_arr(&self, indx: usize, indy: usize) -> f64 {
+    fn depth_from_arr(&self, indx: &usize, indy: &usize) -> f64 {
         let index = self.variables.0.len() * indy + indx;
         self.variables.2[index]
     }
@@ -189,7 +189,7 @@ impl CartesianFile {
     /// `interpolate` panic.
     fn depth(&self, x: &f32, y: &f32) -> f64 {
         let nearest_pt = self.nearest_point(x, y);
-        let edge_points = self.four_corners(nearest_pt.0, nearest_pt.1);
+        let edge_points = self.four_corners(&nearest_pt.0, &nearest_pt.1);
         let depth = self.interpolate(&edge_points, &(*x as f64, *y as f64));
         depth
     }
@@ -215,15 +215,15 @@ mod test_cartestian_coor {
     // test the and view the nearest function
     fn test_get_nearest() {
         let data = CartesianFile::new(Path::new("data/test_bathy_3.nc"));
-        dbg!(data.nearest(5499.0, "x"));
-        assert!(data.nearest(5499.0, "x") == 11);
+        dbg!(data.nearest(&5499.0, "x"));
+        assert!(data.nearest(&5499.0, "x") == 11);
     }
 
     #[test]
     // view the output from four_corners function
     fn test_get_corners() {
         let data = CartesianFile::new(Path::new("data/test_bathy_3.nc"));
-        dbg!(data.four_corners(10, 10));
+        dbg!(data.four_corners(&10, &10));
     }
 
     #[test]
