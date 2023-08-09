d962b458a136691319b02deafa0a9f793d82d209
Bryce Irving <bryceirving@mines.edu>
2023-08-09 16:54:56 -0600

    feat: functions to create two test files

--- src/ray.rs
+++ src/ray.rs
@@ -17,15 +17,54 @@ impl SingleRay {
         SingleRay {}
     }
 
-    /// This purely does the ode_solvers Rk4 tracing and saves answer in
-    /// self.result
+    /// computes ode_solvers Rk4 tracing and returns result
     /// 
-    /// #NOTE This takes too many arguments, is confusing, and also
-    /// unoptimized. I need to move bathymetry_data to being a member variable
-    /// of the struct.
+    /// # Arguments
+    /// 
+    /// `bathymetry_data`: `&'b dyn BathymetryData`
+    /// - the data type that implements `get_depth` representing the depth of
+    ///   the area.
+    /// 
+    /// `x0` : `&f64`
+    /// - the initial x position
+    /// 
+    /// `y0` : `&f64`
+    /// - the initial y position
+    /// 
+    /// `kx0` : `&f64`
+    /// - the initial kx value
+    /// 
+    /// `ky0` : `&f64`
+    /// - the initial ky value
+    /// 
+    /// `start_time` : `&f64`
+    /// - time to start the Rk4
+    /// 
+    /// `end_time` :
+    /// - time to end the Rk4
+    /// 
+    /// `step_size` : `&f64`
+    /// - delta t
+    /// 
+    /// # Returns
+    /// `Result<(Vec<f64>, Vec<OVector<f64,
+    /// nalgebra::base::dimension::Const<4>>>), Error>`
+    /// - `Ok((Vec<f64>, Vec<OVector<f64,
+    ///   nalgebra::base::dimension::Const<4>>>))` : a tuple of the x_out and
+    ///   y_out from the Rk4 stepper.
+    /// - `Err(Error::IntegrationError)` : there was an error during Rk4
+    ///   integrate method.
+    /// 
+    /// # Panics
+    /// This function will panic if the wave ever propagates out of bounds of
+    /// the bathymetry data array during Rk4 integration.
+    /// 
+    /// # Note
+    /// This takes too many arguments, is confusing, and also unoptimized. I
+    /// need to move bathymetry_data to being a member variable of the struct.
     pub fn trace_individual<'b>( &self, 
             bathymetry_data: &'b dyn BathymetryData, 
-            x0: f64, y0: f64, kx0: f64, ky0: f64, 
+            x0: &f64, y0: &f64, kx0: &f64, ky0: &f64, 
             start_time: &f64, end_time: &f64,
             step_size: &f64 ) 
             -> Result<(Vec<f64>, Vec<OVector<f64, nalgebra::base::dimension::Const<4>>>), Error>
@@ -33,7 +72,7 @@ impl SingleRay {
 
         // do the calculations
         let system = WaveRayPath::new(bathymetry_data, *step_size);
-        let y0 = State::new(x0, y0, kx0, ky0);
+        let y0 = State::new(*x0, *y0, *kx0, *ky0);
         let mut stepper = Box::new(Rk4::new(system, *start_time, y0, *end_time, *step_size));
         if stepper.integrate().is_ok() {
 
@@ -82,7 +121,7 @@ fn output_to_tsv_file(file_name: &str, x_out: &Vec<f64>, y_out: &Vec<OVector<f64
 
     use super::{SingleRay, output_to_tsv_file};
 
-        /// Create a test file like the test_bathy.nc
+        /// Create a constant depth file
         fn create_constant_depth_file(path: &Path, x_len: usize, y_len: usize, x_step: f32, y_step: f32) {
             
             let x_data: Vec<f32> = (0..x_len).map(|x| x as f32 * x_step).collect();
@@ -133,14 +172,89 @@ fn output_to_tsv_file(file_name: &str, x_out: &Vec<f64>, y_out: &Vec<OVector<f64
 
         }
 
+        /// Create a test file with depths split down the middle
+        fn create_two_depth_file(path: &Path, x_len: usize, y_len: usize, x_step: f32, y_step: f32) {
+            
+            let x_data: Vec<f32> = (0..x_len).map(|x| x as f32 * x_step).collect();
+            let y_data: Vec<f32> = (0..y_len).map(|y| y as f32 * y_step).collect();
+            let depth_data: Vec<f64> = (0..(x_len*y_len))
+                .map(|p| {
+                    if p % x_len >= 50 {
+                        48.0f64
+                    } else {
+                        50.0f64
+                    }
+                }
+            )
+                .collect();
+
+            // most below copied from the docs
+            use netcdf3::{FileWriter, DataSet, Version};
+            let y_dim_name: &str = "y";
+            let y_var_name: &str = y_dim_name;
+            let y_var_len: usize = y_len;
+            
+            let x_dim_name: &str = "x";
+            let x_var_name: &str = x_dim_name;
+            let x_var_len: usize = x_len;
+            
+            let depth_var_name: &str = "depth";
+            let depth_var_len: usize = depth_data.len();
+            
+            // Create the NetCDF-3 definition
+            // ------------------------------
+            let data_set: DataSet = {
+                let mut data_set: DataSet = DataSet::new();
+                // Define the dimensions
+                data_set.add_fixed_dim(y_dim_name, y_var_len).unwrap();
+                data_set.add_fixed_dim(x_dim_name, x_var_len).unwrap();
+                // Define the variable
+                data_set.add_var_f32(y_var_name, &[y_dim_name]).unwrap();
+                data_set.add_var_f32(x_var_name, &[x_var_name]).unwrap();
+                data_set.add_var_f64(depth_var_name, &[y_dim_name, x_var_name]).unwrap();
+            
+                data_set
+            };
+            
+            // ...
+            
+            // Create and write the NetCDF-3 file
+            // ----------------------------------
+            let mut file_writer: FileWriter = FileWriter::open(path).unwrap();
+            // Set the NetCDF-3 definition
+            file_writer.set_def(&data_set, Version::Classic, 0).unwrap();
+            assert_eq!(depth_var_len,                     x_var_len * y_var_len);
+            file_writer.write_var_f32(y_var_name, &y_data[..]).unwrap();
+            file_writer.write_var_f32(x_var_name, &x_data[..]).unwrap();
+            file_writer.write_var_f64(depth_var_name, &depth_data[..]).unwrap();
+             file_writer.close().unwrap();
+            // end of copied from docs
+
+        }
+
+
         #[test]
-        fn test_single_wave() {
+        // this test does not check anything yet, but outputs the result to a space separated file
+        fn test_constant_wave() {
             let wave = SingleRay::new();
         
             let bathymetry_data: &dyn BathymetryData = &cartesian::CartesianFile::new(Path::new("constant_depth.nc"));
         
             // make sure the starting point is at least 2 steps away from the edge.
-            let res = wave.trace_individual(bathymetry_data, 10.0, 50.0, 0.12, 0.0, &0.0, &1.0, &1.0).unwrap();
+            let res = wave.trace_individual(bathymetry_data, &10.0, &50.0, &0.12, &0.0, &0.0, &18.0, &1.0).unwrap();
+        
+            let _ = output_to_tsv_file("y_out.txt",&res.0, &res.1);
+        }
+
+        #[test]
+        // this test does not check anything yet, but outputs the result to a space separated file
+        fn test_two_depth_wave() {
+            let wave = SingleRay::new();
+        
+            let bathymetry_data: &dyn BathymetryData = &cartesian::CartesianFile::new(Path::new("two_depth.nc"));
+        
+            // make sure the starting point is at least 2 steps away from the edge.
+            let res = wave.trace_individual(bathymetry_data, &10.0, &50.0, &0.12, &0.0, &0.0, &18.0, &1.0).unwrap();
         
             let _ = output_to_tsv_file("y_out.txt",&res.0, &res.1);
         }
