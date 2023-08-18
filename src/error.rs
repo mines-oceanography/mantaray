ef804103d1be8ee00bd4ea8922fdfc75e176ac32
Gui Castelao <guilherme@castelao.net>
2023-08-18 12:58:11 -0600

    Using original io::Error
    Transport lower level error for more transparency.

--- src/error.rs
+++ src/error.rs
@@ -1,29 +1,31 @@
 //! Custom enum Error defining ray_tracing specific errors.
-//! 
+//!
 //! The enum depends on thiserror::Error.
 
 #[derive(Debug, thiserror::Error)]
 pub(crate) enum Error {
-   #[error("Undefined error")]
-   /// The undefined error will be used as a placeholder for all other errors.
-   /// Two known errors that are currently undefined but need their own instance
-   /// in the future are an index out of bounds error and an integration error.
-   Undefined,
+    #[error("Undefined error")]
+    /// The undefined error will be used as a placeholder for all other errors.
+    /// Two known errors that are currently undefined but need their own instance
+    /// in the future are an index out of bounds error and an integration error.
+    Undefined,
 
-   #[error("Argument passed was out of bounds")]
-   /// The value k = |(kx, ky)| can only be positive. If k <=0, the function will pass ArgumentOutOfBounds.
-   ArgumentOutOfBounds,
+    #[error("Argument passed was out of bounds")]
+    /// The value k = |(kx, ky)| can only be positive. If k <=0, the function will pass ArgumentOutOfBounds.
+    ArgumentOutOfBounds,
 
-   #[error("Argument passed was not a valid option")]
-   /// The argument passed was not a valid option
-   InvalidArgument,
-   
-   #[error("Index passed was out of bounds")]
-   /// The index is out of bounds of the array and would panic if attempted to access array.
-   IndexOutOfBounds,
-   
-   #[error("Error during `ode_solvers` integration")]
-   /// There was an error during integration using `ode_solvers`
-   IntegrationError,
+    #[error("Argument passed was not a valid option")]
+    /// The argument passed was not a valid option
+    InvalidArgument,
 
+    #[error("Index passed was out of bounds")]
+    /// The index is out of bounds of the array and would panic if attempted to access array.
+    IndexOutOfBounds,
+
+    #[error("Error during `ode_solvers` integration")]
+    /// There was an error during integration using `ode_solvers`
+    IntegrationError,
+
+    #[error(transparent)]
+    IOError(#[from] std::io::Error),
 }
