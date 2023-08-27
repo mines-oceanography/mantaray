19f0522f9b6e691dcd9bd0f2aa7282a43f414049
Gui Castelao <guilherme@castelao.net>
2023-08-26 20:18:39 -0600

    test: ConstantDepth with default h

--- src/bathymetry.rs
+++ src/bathymetry.rs
@@ -39,9 +39,15 @@ mod test_constantdepth {
     use super::{ConstantDepth, ConstantDepthBuilder};
 
     #[test]
-    fn build() {
+    fn build_default() {
         let c = ConstantDepthBuilder::default().build().unwrap();
         assert_eq!(c, ConstantDepth { h: 1000.0 });
+    }
+
+    #[test]
+    fn build() {
+        let c = ConstantDepthBuilder::default().h(42.0).build().unwrap();
+        assert_eq!(c, ConstantDepth { h: 42.0 });
     }
 }
 
