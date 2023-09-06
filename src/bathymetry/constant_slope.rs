a7933a8abd956df83f0c6f78a34df13b35d1338e
Guilherme Castelão <guilherme@castelao.net>
2023-09-05 22:20:53 -0600

    feat: New type of bathymetry - constant slope (#32)
    * feat: New type of bathymetry - constant slope
    
    It has an infinite domain with given constant slope (dx, dy), and
    reference depth (h0) for a given location (x0, y0).
    
    This is another application for idealized scenarios. With it we can
    avoid creating a bathymetry file as well as avoid I/O. This will be
    particularly useful for tests, reducing the computational cost.
    
    * fix: Missing h0 term
    
    Thanks to @brycemines.
    
    * typo: Fixing references to constant slope
    
    I copied from constant depth, so fixing the reference to what should be
    constant slope.
    
    * Default for constant depth
    
    h0=50, dhdx=0.05, thus coastline is 1000m away.
    
    * fix: dhdx & dhdy instead of dx & dy
    
    * Fixing tests and default values
    
    Slope should be negative to reach coastline in a positive x.
    
    * A patch upgrade
    
    New type of bathymetry - constant slope.
    
    * doc: Minimal documentation for ConstantDepth

---
+++ src/bathymetry/constant_slope.rs
@@ -0,0 +1,147 @@
+use super::BathymetryData;
+use crate::error::Error;
+use derive_builder::Builder;
+
+#[derive(Builder, Debug, PartialEq)]
+/// A bathymetry pseudo-database with constant slope
+///
+/// # Arguments
+///
+/// * `h0`: Depth [m] at ($x_0$, $y_0$).
+/// * `x0`: `x` coordinate [m] where depth is `h0` at `y0`.
+/// * `y0`: `y` coordinate [m] where depth is `h0` at `x0`.
+/// * `dhdx`: Slope on x direction.
+/// * `dhdy`: Slope on y direction.
+///
+/// ConstantSlope can be build with default values, which is a convenient
+/// approach to build tests.
+///
+/// * Manual definition
+/// let bathymetry = ConstantSlope(100, 0, 0, -1e-2, 0);
+///
+/// * Using default values
+/// let h100 = ConstantSlope::builder().h0(100).build().unwrap();
+///
+/// This might be only useful for development and tests.
+pub(crate) struct ConstantSlope {
+    #[builder(default = "50.0")]
+    h0: f32,
+    #[builder(default = "0.0")]
+    x0: f32,
+    #[builder(default = "0.0")]
+    y0: f32,
+    #[builder(default = "-5e-2")]
+    dhdx: f32,
+    #[builder(default = "0.0")]
+    dhdy: f32,
+}
+
+impl BathymetryData for ConstantSlope {
+    /// Depth for a given position (x, y)
+    ///
+    /// Returns NaN when any input is NaN. Since it is a constant slope,
+    /// there is no concept of boundaries, thus it can't fail as out of
+    /// bounds.
+    fn get_depth(&self, x: &f32, y: &f32) -> Result<f32, Error> {
+        if x.is_nan() || y.is_nan() {
+            Ok(f32::NAN)
+        } else {
+            Ok(self.h0 + self.dhdx * (x - self.x0) + self.dhdy * (y - self.y0))
+        }
+    }
+
+    /// Depth and gradient for a given position (x, y)
+    ///
+    /// Returns NaN when any input is NaN. Since it is a constant slope,
+    /// there is no concept of boundaries, thus it can't fail as out of
+    /// bounds.
+    fn get_depth_and_gradient(&self, x: &f32, y: &f32) -> Result<(f32, (f32, f32)), Error> {
+        if x.is_nan() || y.is_nan() {
+            Ok((f32::NAN, (f32::NAN, f32::NAN)))
+        } else {
+            let h = self.h0 + self.dhdx * (x - self.x0) + self.dhdy * (y - self.y0);
+            Ok((h, (self.dhdx, self.dhdy)))
+        }
+    }
+}
+
+impl ConstantSlope {
+    #[allow(dead_code)]
+    fn builder() -> ConstantSlopeBuilder {
+        ConstantSlopeBuilder::default()
+    }
+}
+
+#[cfg(test)]
+mod test_constant_slope {
+    use super::{BathymetryData, ConstantSlope};
+
+    #[test]
+    // Maybe this is not clear for constant slope, but it should respect the
+    // general behavior for other types of bathymetries. A NaN input is not
+    // an error per se, but should result in a NaN result. Different than
+    // an out of bounds request or unfeasible lat/lon coordinate.
+    fn nan_input() {
+        let c = ConstantSlope {
+            h0: 100.0,
+            x0: 0.0,
+            y0: 0.0,
+            dhdx: -1e-2,
+            dhdy: 0.0,
+        };
+
+        assert!(c.get_depth(&f32::NAN, &0.0).unwrap().is_nan());
+        assert!(c.get_depth(&0.0, &f32::NAN).unwrap().is_nan());
+        assert!(c.get_depth(&f32::NAN, &f32::NAN).unwrap().is_nan());
+    }
+}
+
+#[cfg(test)]
+mod test_builder {
+    use super::{ConstantSlope, ConstantSlopeBuilder};
+
+    #[test]
+    fn build_default() {
+        let c = ConstantSlopeBuilder::default().build().unwrap();
+        assert_eq!(
+            c,
+            ConstantSlope {
+                h0: 50.0,
+                x0: 0.0,
+                y0: 0.0,
+                dhdx: -5e-2,
+                dhdy: 0.0
+            }
+        );
+    }
+
+    #[test]
+    fn build() {
+        let c = ConstantSlopeBuilder::default().h0(42.0).build().unwrap();
+        assert_eq!(
+            c,
+            ConstantSlope {
+                h0: 42.0,
+                x0: 0.0,
+                y0: 0.0,
+                dhdx: -5e-2,
+                dhdy: 0.0
+            }
+        );
+    }
+
+    #[test]
+    fn builder() {
+        let c = ConstantSlope::builder().build().unwrap();
+        assert_eq!(
+            c,
+            ConstantSlope {
+                h0: 50.0,
+                x0: 0.0,
+                y0: 0.0,
+                dhdx: -5e-2,
+                dhdy: 0.0
+            }
+        );
+    }
+}
