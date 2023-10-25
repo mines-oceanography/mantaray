23b8784dff9e054a030677a6444931f6997cf5d3
Bryce Irving <bryceirving@mines.edu>
2023-10-25 08:55:39 -0600

    fix: remove Sync for now from CurrentData

--- src/current/mod.rs
+++ src/current/mod.rs
@@ -5,6 +5,6 @@ mod constant_current;
 #[allow(unused_imports)]
 pub(super) use constant_current::ConstantCurrent;
 
-pub(crate) trait CurrentData : Sync {
+pub(crate) trait CurrentData {
     fn get_current(&self, x: &f32, y: &f32) -> Result<f32, Error>;
 }
