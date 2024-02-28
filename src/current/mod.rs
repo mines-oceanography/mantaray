16fcf8c2e0ed089ca78a54817fa890f3bab698fa
Bryce Irving <bryceirving@mines.edu>
2024-02-28 12:23:44 -0700

    feat: add cartesian.rs to current module

--- src/current/mod.rs
+++ src/current/mod.rs
@@ -6,14 +6,21 @@
 
 use crate::error::Error;
 
+mod cartesian;
 mod constant_current;
 
 #[allow(unused_imports)]
+pub(super) use cartesian::Cartesian;
+#[allow(unused_imports)]
 pub(super) use constant_current::ConstantCurrent;
 
-pub(crate) trait CurrentData {
+pub(crate) trait CurrentData: Sync {
     /// Return the current (u, v) at the given (x, y)
     fn current(&self, x: &f64, y: &f64) -> Result<(f64, f64), Error>;
     /// Return the current (u, v) and the gradient (du/dx, du/dy, dv/dx, dv/dy)
-    fn current_and_gradient(&self, x: &f64, y: &f64) -> Result<((f64, f64), (f64, f64, f64, f64)), Error>;
+    fn current_and_gradient(
+        &self,
+        x: &f64,
+        y: &f64,
+    ) -> Result<((f64, f64), (f64, f64, f64, f64)), Error>;
 }
