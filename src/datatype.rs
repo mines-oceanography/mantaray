305330d7913e9e80cddea7417133df75bed15def
Gui Castelao <guilherme@castelao.net>
2024-03-24 17:59:00 -0600

    Moving data types into its own module

---
+++ src/datatype.rs
@@ -0,0 +1,104 @@
+//! # Data types
+
+/// A point in 2D cartesian space
+///
+/// A `Point` is composed by `x` and `y`, expected to be in meters.
+pub(crate) struct Point<T> {
+    x: T,
+    y: T,
+}
+
+#[allow(dead_code)]
+impl<T> Point<T> {
+    /// Create a new `Point` with the given `x` and `y` coordinates.
+    ///
+    /// # Examples
+    /// ```
+    /// let p = Point::new(1.0, 2.0);
+    /// assert_eq!(*p.x(), 1.0);
+    /// assert_eq!(*p.y(), 2.0);
+    /// ```
+    fn new(x: T, y: T) -> Self {
+        Point { x, y }
+    }
+
+    /// Get the `x` coordinate of the `Point`.
+    ///
+    /// # Examples
+    /// ```
+    /// let p = Point::new(1.0, 2.0);
+    /// assert_eq!(*p.x(), 1.0);
+    /// ```
+    fn x(&self) -> &T {
+        &self.x
+    }
+
+    /// Get the `y` coordinate of the `Point`.
+    ///
+    /// # Examples
+    /// ```
+    /// let p = Point::new(1.0, 2.0);
+    /// assert_eq!(*p.y(), 2.0);
+    /// ```
+    fn y(&self) -> &T {
+        &self.y
+    }
+}
+
+/// A 2D geolocation in a 2D space
+///
+/// A `Coordinate` is composed by `lat` and `lon`, expected to be in decimal
+/// degrees. For instance, the latitude of the North Pole is 90, and a
+/// latitude of -10.5 is equivalent to 10 degrees and 30 minutes South.
+pub(crate) struct Coordinate<T> {
+    lat: T,
+    lon: T,
+}
+
+#[allow(dead_code)]
+impl<T> Coordinate<T> {
+    /// Create a new `Coordinate` with the given `lat` and `lon` coordinates.
+    ///
+    /// # Examples
+    /// ```
+    /// let c = Coordinate::new(1.0, 2.0);
+    /// assert_eq!(*c.lat(), 1.0);
+    /// assert_eq!(*c.lon(), 2.0);
+    /// ```
+    fn new(lon: T, lat: T) -> Self {
+        Coordinate { lat, lon }
+    }
+
+    fn lat(&self) -> &T {
+        &self.lat
+    }
+
+    fn lon(&self) -> &T {
+        &self.lon
+    }
+}
+
+#[allow(dead_code)]
+/// The current in a 2D cartesian point
+///
+/// A `Current` is composed by `u` and `v`, expected to be in meters per
+/// second.
+pub(crate) struct Current<T> {
+    u: T,
+    v: T,
+}
+
+#[allow(dead_code)]
+impl<T> Current<T> {
+    fn new(u: T, v: T) -> Self {
+        Current { u, v }
+    }
+
+    fn u(&self) -> &T {
+        &self.u
+    }
+
+    fn v(&self) -> &T {
+        &self.v
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -25,109 +25,11 @@
 
 mod bathymetry;
 mod current;
+mod datatype;
 mod error;
 mod interpolator;
 mod ray;
 mod wave_ray_path;
 
-/// A point in 2D cartesian space
-///
-/// A `Point` is composed by `x` and `y`, expected to be in meters.
-struct Point<T> {
-    x: T,
-    y: T,
-}
-
-#[allow(dead_code)]
-impl<T> Point<T> {
-    /// Create a new `Point` with the given `x` and `y` coordinates.
-    ///
-    /// # Examples
-    /// ```
-    /// let p = Point::new(1.0, 2.0);
-    /// assert_eq!(*p.x(), 1.0);
-    /// assert_eq!(*p.y(), 2.0);
-    /// ```
-    fn new(x: T, y: T) -> Self {
-        Point { x, y }
-    }
-
-    /// Get the `x` coordinate of the `Point`.
-    ///
-    /// # Examples
-    /// ```
-    /// let p = Point::new(1.0, 2.0);
-    /// assert_eq!(*p.x(), 1.0);
-    /// ```
-    fn x(&self) -> &T {
-        &self.x
-    }
-
-    /// Get the `y` coordinate of the `Point`.
-    ///
-    /// # Examples
-    /// ```
-    /// let p = Point::new(1.0, 2.0);
-    /// assert_eq!(*p.y(), 2.0);
-    /// ```
-    fn y(&self) -> &T {
-        &self.y
-    }
-}
-
-/// A 2D geolocation in a 2D space
-///
-/// A `Coordinate` is composed by `lat` and `lon`, expected to be in decimal
-/// degrees. For instance, the latitude of the North Pole is 90, and a
-/// latitude of -10.5 is equivalent to 10 degrees and 30 minutes South.
-struct Coordinate<T> {
-    lat: T,
-    lon: T,
-}
-
-#[allow(dead_code)]
-impl<T> Coordinate<T> {
-    /// Create a new `Coordinate` with the given `lat` and `lon` coordinates.
-    ///
-    /// # Examples
-    /// ```
-    /// let c = Coordinate::new(1.0, 2.0);
-    /// assert_eq!(*c.lat(), 1.0);
-    /// assert_eq!(*c.lon(), 2.0);
-    /// ```
-    fn new(lon: T, lat: T) -> Self {
-        Coordinate { lat, lon }
-    }
-
-    fn lat(&self) -> &T {
-        &self.lat
-    }
-
-    fn lon(&self) -> &T {
-        &self.lon
-    }
-}
-
-/// The current in a 2D cartesian point
-///
-/// A `Current` is composed by `u` and `v`, expected to be in meters per
-/// second.
-struct Current<T> {
-    u: T,
-    v: T,
-}
-
-#[allow(dead_code)]
-impl<T> Current<T> {
-    fn new(u: T, v: T) -> Self {
-        Current { u, v }
-    }
-
-    fn u(&self) -> &T {
-        &self.u
-    }
-
-    fn v(&self) -> &T {
-        &self.v
-    }
-}
+#[allow(unused_imports)]
+use datatype::{Coordinate, Current, Point};
