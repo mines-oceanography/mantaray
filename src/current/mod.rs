090e5ebb44055fd96ec006e1c995bcf10a33d0ef
brycemines <126987327+brycemines@users.noreply.github.com>
2024-04-16 15:28:00 -0600

    feat: add cartesian.rs to current module (#56)
    * feat: add cartesian.rs to current module
    
    * fix: simple doc and rearranging refactors
    
    * fix: split field into multiple vectors for easier use
    
    * fix: use tempfile instead of lockfile
    
    * refactor: rename current/cartesian.rs to current/cartesian_current.rs
    
    * fix: include renamed file in commit
    
    * refactor: rename new to open in cartesian_current

--- src/current/mod.rs
+++ src/current/mod.rs
@@ -6,8 +6,11 @@
 
 use crate::error::Error;
 
+mod cartesian_current;
 mod constant_current;
 
+#[allow(unused_imports)]
+pub(super) use cartesian_current::CartesianCurrent;
 #[allow(unused_imports)]
 pub(super) use constant_current::ConstantCurrent;
 
@@ -15,5 +18,9 @@ pub(crate) trait CurrentData {
     /// Return the current (u, v) at the given (x, y)
     fn current(&self, x: &f64, y: &f64) -> Result<(f64, f64), Error>;
     /// Return the current (u, v) and the gradient (du/dx, du/dy, dv/dx, dv/dy)
-    fn current_and_gradient(&self, x: &f64, y: &f64) -> Result<((f64, f64), (f64, f64, f64, f64)), Error>;
+    fn current_and_gradient(
+        &self,
+        x: &f64,
+        y: &f64,
+    ) -> Result<((f64, f64), (f64, f64, f64, f64)), Error>;
 }
