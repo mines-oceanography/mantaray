02b59a5aa20508c6f2cca5f66413783aa14de554
brycemines <126987327+brycemines@users.noreply.github.com>
2024-05-31 16:13:49 -0600

    Current unit tests (#72)
    * feat: add current to ray tracing single and many
    
    * test: constant current and current with constant gradient

--- tests/linear_sea_mount.rs
+++ tests/linear_sea_mount.rs
@@ -1,5 +1,5 @@
-use std::path::Path;
 use ray_tracing;
+use std::path::Path;
 
 #[cfg(test)]
 #[test]
@@ -7,14 +7,16 @@ use ray_tracing;
 /// should bend towards the circular island such that rays above the island
 /// curve down and rays below the island curve up.
 fn test_sea_mount() {
-    let bathymetry_data = ray_tracing::bathymetry::cartesian::CartesianFile::new(Path::new("tests/data/island_slice.nc"));
+    let bathymetry_data = ray_tracing::bathymetry::cartesian::CartesianFile::new(Path::new(
+        "tests/data/island_slice.nc",
+    ));
 
     // top half
     let initial_waves: Vec<(f64, f64, f64, f64)> = (0..10)
         .map(|v| (-990.0, (v * 100) as f64, 0.01, 0.0))
         .collect();
 
-    let waves = ray_tracing::ray::ManyRays::new(&bathymetry_data, &initial_waves);
+    let waves = ray_tracing::ray::ManyRays::new(&bathymetry_data, None, &initial_waves);
 
     let results = waves.trace_many(0.0, 1000.0, 1.0);
 
@@ -46,7 +48,7 @@ fn test_sea_mount() {
         .map(|v| (-990.0, (v * -100) as f64, 0.01, 0.0))
         .collect();
 
-    let waves = ray_tracing::ray::ManyRays::new(&bathymetry_data, &initial_waves);
+    let waves = ray_tracing::ray::ManyRays::new(&bathymetry_data, None, &initial_waves);
 
     let results = waves.trace_many(0.0, 1000.0, 1.0);
 
