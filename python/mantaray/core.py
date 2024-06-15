2b5bb0e02fabe82b8710f2d44e4af0b914a74f51
Guilherme Castelão <guilherme@castelao.net>
2024-06-15 16:41:13 -0600

    Adding currents to Python interface (#78)
    * Adding current
    
    * Using currents
    
    * Adding dependencies to pixi.toml

--- python/mantaray/core.py
+++ python/mantaray/core.py
@@ -1,8 +1,16 @@
-
 from . import _mantaray
 
 
-def single_ray(x0: float, y0: float, kx0: float, ky0: float, duration: float, step_size: float, filename: str):
+def single_ray(
+    x0: float,
+    y0: float,
+    kx0: float,
+    ky0: float,
+    duration: float,
+    step_size: float,
+    bathymetry: str,
+    current: str,
+):
     """Propagate a single ray without considering the effect of currents
 
     Parameters
@@ -15,5 +23,7 @@ def single_ray(x0: float, y0: float, kx0: float, ky0: float, duration: float, st
     --------
     >>> mantaray.single_ray(-1000, 0, 0.01, 0, 10, 2, "island.nc")
     """
-    output = _mantaray.single_ray(x0, y0, kx0, ky0, duration, step_size, filename)
+    output = _mantaray.single_ray(
+        x0, y0, kx0, ky0, duration, step_size, bathymetry, current
+    )
     return output
