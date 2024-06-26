e559d144f2cf3c72de789b04cb7b55fbd4ab5cc2
Bryce Irving <bryceirving@mines.edu>
2024-06-26 10:32:21 -0600

    test: linear beach along x starting deep water

---
+++ tests/linear_beach.rs
@@ -0,0 +1,121 @@
+use std::f64::consts::PI;
+
+use mantaray::{
+    self, bathymetry::CartesianNetcdf3, current::ConstantCurrent,
+    io::utility::create_netcdf3_bathymetry, ray::ManyRays,
+};
+use tempfile::NamedTempFile;
+
+mod helper;
+use helper::*;
+
+#[test]
+fn test_deep_linear_beach_right() {
+    // create file for the bathymetry data
+    let temp_path = NamedTempFile::new().unwrap().into_temp_path();
+
+    // beach on right
+    create_netcdf3_bathymetry(&temp_path, 200, 100, 500.0, 500.0, |x, _y| {
+        if x < 50_000.0 {
+            2000.0
+        } else {
+            if 2000.0 - 0.05 * (x - 50_000.0) as f64 >= 0.0 {
+                2000.0 - 0.05 * (x - 50_000.0) as f64
+            } else {
+                0.0
+            }
+        }
+    });
+
+    let bathymetry_data = CartesianNetcdf3::open(&temp_path, "x", "y", "depth").unwrap();
+    let current_data = ConstantCurrent::new(0.0, 0.0);
+
+    let k = 0.05;
+
+    let up_ray = (
+        10_000.0,
+        1_000.0,
+        k * (PI / 6.0).cos(),
+        k * (PI / 6.0).sin(),
+    );
+
+    let down_ray = (
+        10_000.0,
+        49_000.0,
+        k * (-PI / 6.0).cos(),
+        k * (-PI / 6.0).sin(),
+    );
+
+    let straight_ray = (10_000.0, 25_000.0, k, 0.0);
+
+    let initial_rays = vec![up_ray, down_ray, straight_ray];
+
+    let waves = ManyRays::new(&bathymetry_data, Some(&current_data), &initial_rays);
+
+    let results = waves.trace_many(0.0, 100_000.0, 1.0);
+
+    let mut results_iter = results.iter().flatten();
+
+    // order is up, down, straight
+    let up_result = results_iter.next().unwrap();
+    let down_result = results_iter.next().unwrap();
+    let straight_result = results_iter.next().unwrap();
+    assert!(results_iter.next().is_none());
+
+    // verify up ray
+    let (_, data) = up_result.get();
+    increase(data, XINDEX);
+    increase(data, YINDEX);
+    same(data, KY_INDEX);
+
+    // kx same until beach, where it will be >= to start
+    let mut last_kx = data[0][KX_INDEX];
+    for state in data.iter().filter(|v| !v[0].is_nan()).skip(1) {
+        let x = state[XINDEX];
+        let kx = state[KX_INDEX];
+        if x < 50_000.0 {
+            assert_eq!(last_kx, kx);
+            last_kx = kx;
+        } else {
+            assert!(kx >= last_kx);
+        }
+    }
+
+    // verify the down ray
+    let (_, data) = down_result.get();
+    increase(data, XINDEX);
+    decrease(data, YINDEX);
+    same(data, KY_INDEX);
+
+    // kx same until beach, where it will be >= to start
+    let mut last_kx = data[0][KX_INDEX];
+    for state in data.iter().filter(|v| !v[0].is_nan()).skip(1) {
+        let x = state[XINDEX];
+        let kx = state[KX_INDEX];
+        if x < 50_000.0 {
+            assert_eq!(last_kx, kx);
+            last_kx = kx;
+        } else {
+            assert!(kx >= last_kx);
+        }
+    }
+
+    // verify the straight ray
+    let (_, data) = straight_result.get();
+    increase(data, XINDEX);
+    same(data, YINDEX);
+    same(data, KY_INDEX);
+
+    // kx same until beach, where it will be >= to start
+    let mut last_kx = data[0][KX_INDEX];
+    for state in data.iter().filter(|v| !v[0].is_nan()).skip(1) {
+        let x = state[XINDEX];
+        let kx = state[KX_INDEX];
+        if x < 50_000.0 {
+            assert_eq!(last_kx, kx);
+            last_kx = kx;
+        } else {
+            assert!(kx >= last_kx);
+        }
+    }
+}
